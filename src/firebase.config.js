eb44ea90447e93148e3be57de94d2de8d26321ae
Bautista <juanbautistadisanto@gmail.com>
2022-11-19 16:04:17 -0300

    change firebase config

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
     projectId: "research-3aea0",
     storageBucket: "research-3aea0.appspot.com",
     messagingSenderId: "263738423354",
-    appId: "1:263738423354:web:d39753b3fc72d84a5b5d03"
+    appId: process.env.REACT_APP_APP_ID,
   };
 
 const app = initializeApp(firebaseConfig);
